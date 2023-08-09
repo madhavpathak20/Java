3d54b25676021cf1fcff83b67c56eac7237851d0
Madhav Pathak <122793263+madhavpathak20@users.noreply.github.com>
2023-08-09 07:06:13 +0530

    Add files via upload

---
+++ area_of_circle.java
@@ -0,0 +1,8 @@
+public class area_of_circle {
+    public static void main(String args[]) {
+        double x = 3.14;
+        int r = 2;
+        double circle = x*r*r;
+        System.out.println("circle="+circle);
+    }
+}
