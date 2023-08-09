d67e519185466ac40c5797b1ea556d5496eee527
Madhav Pathak <122793263+madhavpathak20@users.noreply.github.com>
2023-08-09 07:06:35 +0530

    Add files via upload

---
+++ check_number.java
@@ -0,0 +1,12 @@
+public class check_number {
+    public static void main(String args[]) {
+        int a = 15;
+        a=18;
+        if (a%3==0 & a%5==0){
+            System.out.println("yes number is divisible by 3 & 5");
+        }
+        else {
+            System.out.println("number is not divisible by 3 & 5");
+        }
+    }
+}
