2e10c381e308a4a1db55e9cc1b4fe2fc76564a8c
Madhav Pathak <122793263+madhavpathak20@users.noreply.github.com>
2023-08-09 07:06:54 +0530

    Add files via upload

---
+++ simple_interest.java
@@ -0,0 +1,10 @@
+public class simple_interest {
+    public static void main (String args []) {
+        int p = 10000;
+        int r = 10;
+        int t = 1;
+        double interest = p*r*t/100;
+        System.out.print("Interest="+interest);
+
+    }
+}
