8ded2902f316387488645f39e5765c28ecf1475a
Madhav Pathak <122793263+madhavpathak20@users.noreply.github.com>
2023-08-09 07:05:44 +0530

    Add files via upload

---
+++ Even_odd.java
@@ -0,0 +1,13 @@
+public class Even_odd {
+    public static void main(String args[]){
+        int a = 16;
+        a=15;
+        if(a%2==0){
+            System.out.println("Even");
+        }
+        else {
+            System.out.println("odd");
+        }
+
+    }
+}
